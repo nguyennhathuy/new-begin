4070d3d7644f384261e9ecc41ce38821a99d5d38
Huy Nguyen <nhathuy120796@gmail.com>
2023-11-29 23:52:27 +0700

    daily commit 4

---
+++ lesson7/src/lesson7/test.java
@@ -0,0 +1,10 @@
+package lesson7;
+
+public class test {
+	public static void main(String[] args) {
+		Animal meo = new Cat("Xy", 3);
+		Animal cho = new Dog("Dua", 4);
+		meo.tiengKeu();
+		cho.tiengKeu();
+	}
+}
